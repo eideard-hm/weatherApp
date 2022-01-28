8768f322b305f1da8bb2a835d2c5c05a5cd29dd8
eideard-hm <ehhernandez81@misena.edu.co>
2022-01-28 09:21:22 -0500

    Inital config and files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
+import { WeatherModule } from './pages/weather/weather.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchModule } from './components/search/search.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SearchModule,
+    WeatherModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
